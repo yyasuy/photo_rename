afbeb9aa2c6adb0bd6002b1cf62f5f2236c022f9
Yasuhiro Yamanaka <yasuhiro@Yasuhiro-no-MacBook.local>
2018-07-05 12:51:32 +0900

    Fix bug.

--- photo_rename.php
+++ photo_rename.php
@@ -1,11 +1,11 @@
 <?php
-define( $TARGET_DIRS,	array(
+define( TARGET_DIRS,	array(
 				'/Users/yasuhiro/Dropbox/カメラアップロード/',
 				'/Users/yasuhiro/Dropbox/スクリーンショット/',
-			);
+			)
 );
 	
-foreach( $TARGET_DIRS as $dir ){
+foreach( TARGET_DIRS as $dir ){
 	$files = scandir( $dir );
 	foreach( $files as $file ){
 		$pattern = '|[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9\s].+|'; // e.g. 2018-01-23 04 05 06.jpg
